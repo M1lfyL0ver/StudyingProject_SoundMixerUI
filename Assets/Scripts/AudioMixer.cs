1f1e9c81304edd31bf2f91d3133fa27b954afd62
M1lfyL0ver <pibolshe@bk.ru>
2025-10-04 13:38:21 +0700

    v1.0.1

--- Assets/Scripts/AudioMixer.cs
+++ Assets/Scripts/AudioMixer.cs
@@ -4,56 +4,99 @@ using UnityEngine.Audio;
 
 public class AudioMixerController : MonoBehaviour
 {
-    [Header("Audio Mixer Reference")]
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Toggle muteToggle;
+    [SerializeField] private string volumeParameter;
+    [SerializeField] private float minVolumeDB = -80f;
+    [SerializeField] private float volumeThreshold = 0.001f;
 
-    [Header("Slider References")]
-    [SerializeField] private Slider masterSlider;
-    [SerializeField] private Slider musicSlider;
-    [SerializeField] private Slider buttonSlider;
-
-    [Header("Mixer Parameters Names")]
-    [SerializeField] private string masterVolumeParam = "Master";
-    [SerializeField] private string musicVolumeParam = "Music";
-    [SerializeField] private string buttonsVolumeParam = "Buttons";
+    private float currentVolume = 1f;
+    private bool isMuted = false;
 
     private void Start()
     {
-        SetMasterVolume(masterSlider.value);
-        SetMusicVolume(musicSlider.value);
-        SetButtonVolume(buttonSlider.value);
+        InitializeUI();
     }
 
     private void OnEnable()
     {
-        masterSlider.onValueChanged.AddListener(SetMasterVolume);
-        musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        buttonSlider.onValueChanged.AddListener(SetButtonVolume);
+        if (volumeSlider != null)
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+
+        if (muteToggle != null)
+            muteToggle.onValueChanged.AddListener(ToggleMute);
     }
 
     private void OnDisable()
     {
-        masterSlider.onValueChanged.RemoveAllListeners();
+        if (volumeSlider != null)
+            volumeSlider.onValueChanged.RemoveListener(SetVolume);
+
+        if (muteToggle != null)
+            muteToggle.onValueChanged.RemoveListener(ToggleMute);
     }
 
-    public void SetMasterVolume(float volume)
+    private void InitializeUI()
     {
-        SetVolume(masterVolumeParam, volume);
+        if (volumeSlider != null)
+        {
+            volumeSlider.SetValueWithoutNotify(currentVolume);
+        }
+
+        if (muteToggle != null)
+        {
+            muteToggle.SetIsOnWithoutNotify(isMuted);
+        }
+
+        ApplyVolumeToMixer(currentVolume);
     }
 
-    public void SetMusicVolume(float volume)
+    public void SetVolume(float volume)
     {
-        SetVolume(musicVolumeParam, volume);
+        currentVolume = Mathf.Clamp01(volume);
+
+        if (!isMuted)
+        {
+            ApplyVolumeToMixer(currentVolume);
+        }
+
+        if (muteToggle != null && currentVolume <= volumeThreshold && !isMuted)
+        {
+            muteToggle.SetIsOnWithoutNotify(true);
+            isMuted = true;
+        }
     }
 
-    public void SetButtonVolume(float volume)
+    public void ToggleMute(bool muted)
     {
-        SetVolume(buttonsVolumeParam, volume);
+        isMuted = muted;
+
+        if (isMuted)
+        {
+            audioMixer.SetFloat(volumeParameter, minVolumeDB);
+        }
+        else
+        {
+            ApplyVolumeToMixer(currentVolume);
+        }
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.interactable = !isMuted;
+        }
     }
 
-    private void SetVolume(string parameterName, float volume)
+    private void ApplyVolumeToMixer(float volume)
     {
-        float volumeDB = volume > 0.001f ? Mathf.Log10(volume) * 20 : -80f;
-        audioMixer.SetFloat(parameterName, volumeDB);
+        float volumeDB = volume > volumeThreshold ?
+            Mathf.Log10(volume) * 20f :
+            minVolumeDB;
+
+        audioMixer.SetFloat(volumeParameter, volumeDB);
     }
+
+    public void Mute() => ToggleMute(true);
+    public void Unmute() => ToggleMute(false);
+    public void SetVolumeParameter(string parameter) => volumeParameter = parameter;
 }
